9c943c569ab2caeb061f9055888d05ca5e104380
kw143 <kw142143@gmail.com>
2019-06-15 17:29:58 -0400

    Created home page and linked with creat player page using bottom navigation bar and fragment; cleared up the layout of creat player page

--- app/src/main/java/edu/gatech/cs2340/team/imperialtrader/views/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team/imperialtrader/views/ConfigurationActivity.java
@@ -1,4 +1,43 @@
 package edu.gatech.cs2340.team.imperialtrader.views;
 
-public class ConfigurationActivity {
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import edu.gatech.cs2340.team.imperialtrader.R;
+
+public class ConfigurationActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    setContentView(R.layout.activity_main);
+                    return true;
+                case R.id.create_player:
+                    mTextMessage.setText(R.string.title_create_player);
+                    setContentView(R.layout.create_player);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_player);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
 }
--- app/src/main/java/edu/gatech/cs2340/team/imperialtrader/views/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team/imperialtrader/views/MainActivity.java
@@ -1,44 +1,51 @@
 package edu.gatech.cs2340.team.imperialtrader.views;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import edu.gatech.cs2340.team.imperialtrader.R;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFrag());
     }
 
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch(menuItem.getItemId()) {
+
+            case R.id.navigation_home:
+                fragment = new HomeFrag();
+                break;
+            case R.id.create_player:
+                fragment = new CreatePlayerFrag();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 }
