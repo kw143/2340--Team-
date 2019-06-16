9bcc45304f851910265cf3aff2f75c050f05cbc5
kw143 <kw142143@gmail.com>
2019-06-16 18:35:40 -0400

    added an exit button

--- app/src/main/java/edu/gatech/cs2340/team/imperialtrader/views/HomeFrag.java
+++ app/src/main/java/edu/gatech/cs2340/team/imperialtrader/views/HomeFrag.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import edu.gatech.cs2340.team.imperialtrader.R;
 
@@ -15,6 +16,16 @@ public class HomeFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home, null);
+        View view = inflater.inflate(R.layout.home,
+                container, false);
+        Button btn1 = view.findViewById(R.id.ex);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+        return view;
     }
 }
