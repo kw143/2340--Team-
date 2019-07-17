6bbc4224bf3470526e0881d869454ae7cf08f3f9
awang302 <awang302@gatech.edu>
2019-07-17 00:41:41 -0400

    added junit test

---
+++ app/src/test/java/edu/gatech/cs2340/team/imperialtrader/views/TradeFragTest.java
@@ -0,0 +1,14 @@
+package edu.gatech.cs2340.team.imperialtrader.views;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TradeFragTest {
+
+    @Test
+    public void testBuy() {
+
+        TradeFrag underTest = new TradeFrag();
+    }
+}
